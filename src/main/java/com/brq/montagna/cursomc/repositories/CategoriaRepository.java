2547107bddf48f86bc1672ad905b04415c5bcfec
Henrique La Montagna <henriquelamontagna@gmail.com>
2020-03-24 17:09:33 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/brq/montagna/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.brq.montagna.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brq.montagna.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}
